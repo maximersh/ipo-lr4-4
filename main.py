7f075aab06d0621f6b7c948355a127196cb52771
Максим <maximers6e@gmail.com>
2024-12-10 19:20:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 n = int(input("Введите число n: "))
 
 # Создание генератора списка
-multiples_of_5 = [i for i in range(1, n + 1) if i % 5 == 0]
+spiis5 = [i for i in range(1, n + 1) if i % 5 == 0]
 
 # Вывод списка чисел, которые делятся на 5
-print(multiples_of_5)
+print(spiis5)
